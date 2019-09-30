39b8c5e6b2a9814a6cdd96bc3f9887e2ac1af4b8
Dipesh Suwal <dipeshsuwal@gmail.com>
2019-09-30 22:29:47 +0200

    Update statuspage.js

--- js/statuspage.js
+++ js/statuspage.js
@@ -2,37 +2,44 @@ function updateStatus() {
     let col = 2;
     $status = {
         'ANTENNA NAME': [1, 0],
-
-        'TIME': [1, 1],
-        'TRACKINGSTATE': [1, 7],
-        'TEMP': [1, 5],
-        'PRESSURE': [3, 7],
-        'HUMIDITY': [2, 5],
-        'SOURCE': [1, 6],
-
-        'NEXTTIME': [2, 2],
         'SCHEDULE': [3, 3],
         'LOG': [3, 5],
+        
+        'TIME': [1, 1],
+        'NEXTTIME': [2, 2],
+        'HALT':[1,3],
+        
+        'SOURCE': [1, 6],
+        'TRACKINGSTATE': [1, 7],
+        'AZIMUTH': [4, 11],
+        'ELEVATION': [4, 13],
+
+        'RA': [2, 7],
         'DEC': [3, 9],
         'CATALOGYEAR': [3, 10],
+        
         'IFA': [5, 4],
         'IFB': [5, 5],
         'IFC': [5, 6],
         'IFD': [5, 7],
+        
         'CABLE': [4, 9],
-        'RA': [2, 7],
-        'AZIMUTH': [4, 11],
-        'ELEVATION': [4, 13],
+        'TEMPERATURE': [1, 5],
+        'HUMIDITY': [2, 5],
+        'PRESSURE': [3, 7],
         'WINDSPEED': [5, 9],
         'WINDDIRECTION': [5, 11],
+        
+        'MODE': [3,0],
+        'RATE': [3,1],
         'CHECKS': [6, 0]
     };
-    $.mobile.loading('show', {
-        text: 'Loading',
-        textVisible: true,
-        theme: 'z',
-        html: ""
-    });
+   // $.mobile.loading('show', {
+   //     text: 'Loading',
+    //    textVisible: true,
+    //    theme: 'z',
+   //     html: ""
+  //  });
     // $.get('https://cors-anywhere.herokuapp.com/' + 'https://vlbisysmon.evlbi.wettzell.de/monitoring_archive/fs_web_pages/wettzell/SystemStatusMonitor.html', function(data) {
     //     $ht = $.parseHTML(data);
     //     console.log($ht);
@@ -78,10 +85,33 @@ function updateStatus() {
                 // $('#' + ele + ' span').text($table.children[element[0]].children[element[1]].textContent);
                 $block = $('<div>', { class: 'ui-block-' + block_letter });
                 $grid.append($block);
+                
+                if ($table.children[element[0]].children[element[1]].textContent.localeCompare("  WETTZELL")==0)
+                    {
+                    //console.log('Green');
+                    $block.append($(`<div style="color:green" class="ui-bar ui-bar-a" id=${ele}>${ele}: 
+                                    <span id="" class="ui-li-count">${$table.children[element[0]].children[element[1]].textContent} </span>
+                                </div>`));
+                    }
+                
+                else if ($table.children[element[0]].children[element[1]].textContent.localeCompare("  HALT")==0)
+                    {
+                    //console.log('Green');
+                    $block.append($(`<div style="color:red" class="ui-bar ui-bar-a" id=${ele}>${ele}: 
+                                    <span id="" class="ui-li-count">${$table.children[element[0]].children[element[1]].textContent} </span>
+                                </div>`));
+                    }
+                
+                else{
 
-                $block.append($(`<div class="ui-bar ui-bar-a" id=${ele}>${ele}: <span id="" class="ui-li-count">${$table.children[element[0]].children[element[1]].textContent} </span></div>`));
+                $block.append($(`<div class="ui-bar ui-bar-a" id=${ele}>${ele}: 
+                                    <span id="" class="ui-li-count">${$table.children[element[0]].children[element[1]].textContent} </span>
+                                </div>`));
+                }
+
+                
             }
         }
-        $.mobile.loading("hide");
+    //    $.mobile.loading("hide");
     });
 }

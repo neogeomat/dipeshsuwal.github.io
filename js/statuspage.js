d771c336e2c13339ebfb91c5a1075609337b26ab
amritkarma <amrit.im@gmail.com>
2019-11-14 14:20:07 +0100

    proxy removed

--- js/statuspage.js
+++ js/statuspage.js
@@ -4,11 +4,11 @@ function updateStatus() {
         'ANTENNA NAME': [1, 0],
         'SCHEDULE': [3, 3],
         'LOG': [3, 5],
-        
+
         'TIME': [1, 1],
         'NEXTTIME': [2, 2],
-        'HALT':[1,3],
-        
+        'HALT': [1, 3],
+
         'SOURCE': [1, 6],
         'TRACKINGSTATE': [1, 7],
         'AZIMUTH': [4, 11],
@@ -17,39 +17,40 @@ function updateStatus() {
         'RA': [2, 7],
         'DEC': [3, 9],
         'CATALOGYEAR': [3, 10],
-        
+
         'IFA': [5, 4],
         'IFB': [5, 5],
         'IFC': [5, 6],
         'IFD': [5, 7],
-        
+
         'CABLE': [4, 9],
         'TEMPERATURE': [1, 5],
         'HUMIDITY': [2, 5],
         'PRESSURE': [3, 7],
         'WINDSPEED': [5, 9],
         'WINDDIRECTION': [5, 11],
-        
-        'MODE': [3,0],
-        'RATE': [3,1],
+
+        'MODE': [3, 0],
+        'RATE': [3, 1],
         'CHECKS': [6, 0]
     };
-   // $.mobile.loading('show', {
-   //     text: 'Loading',
+    // $.mobile.loading('show', {
+    //     text: 'Loading',
     //    textVisible: true,
     //    theme: 'z',
-   //     html: ""
-  //  });
+    //     html: ""
+    //  });
     // $.get('https://cors-anywhere.herokuapp.com/' + 'https://vlbisysmon.evlbi.wettzell.de/monitoring_archive/fs_web_pages/wettzell/SystemStatusMonitor.html', function(data) {
     //     $ht = $.parseHTML(data);
     //     console.log($ht);
     // })
-    $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('https://vlbisysmon.evlbi.wettzell.de/monitoring_archive/fs_web_pages/wettzell/SystemStatusMonitor.html') + '&callback=?', function(data) {
-        // alert(data.contents);
-
-        $w = data.contents;
-        $ht = $.parseHTML($w);
-        $table = $ht[1]["children"][0];
+    $.get('pages/SystemStatusMonitor.html', function(data) {
+        // alert(data);
+        // debugger;
+        // $w = data.contents;
+        // $w = $.parseHTML(data);
+        let $ht = $.parseHTML(data);
+        let $table = $ht[5]["children"][0];
 
         // console.log($table.children[row].children[col].textContent);
         $content = $('#StatusPage');
@@ -85,33 +86,27 @@ function updateStatus() {
                 // $('#' + ele + ' span').text($table.children[element[0]].children[element[1]].textContent);
                 $block = $('<div>', { class: 'ui-block-' + block_letter });
                 $grid.append($block);
-                
-                if ($table.children[element[0]].children[element[1]].textContent.localeCompare("  WETTZELL")==0)
-                    {
+
+                if ($table.children[element[0]].children[element[1]].textContent.localeCompare("  WETTZELL") == 0) {
                     //console.log('Green');
                     $block.append($(`<div style="color:green" class="ui-bar ui-bar-a" id=${ele}>${ele}: 
                                     <span id="" class="ui-li-count">${$table.children[element[0]].children[element[1]].textContent} </span>
                                 </div>`));
-                    }
-                
-                else if ($table.children[element[0]].children[element[1]].textContent.localeCompare("  HALT")==0)
-                    {
+                } else if ($table.children[element[0]].children[element[1]].textContent.localeCompare("  HALT") == 0) {
                     //console.log('Green');
                     $block.append($(`<div style="color:red" class="ui-bar ui-bar-a" id=${ele}>${ele}: 
                                     <span id="" class="ui-li-count">${$table.children[element[0]].children[element[1]].textContent} </span>
                                 </div>`));
-                    }
-                
-                else{
+                } else {
 
-                $block.append($(`<div class="ui-bar ui-bar-a" id=${ele}>${ele}: 
+                    $block.append($(`<div class="ui-bar ui-bar-a" id=${ele}>${ele}: 
                                     <span id="" class="ui-li-count">${$table.children[element[0]].children[element[1]].textContent} </span>
                                 </div>`));
                 }
 
-                
+
             }
         }
-    //    $.mobile.loading("hide");
+        //    $.mobile.loading("hide");
     });
 }
